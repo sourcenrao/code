89782eaaabf5e1a55b6606afa432f0528f906541
Nikhil Rao <sourcenrao@gmail.com>
2021-05-17 18:16:27 -0400

    L1.1 Attempt 1

--- ch1/1.1/client.js
+++ ch1/1.1/client.js
@@ -9,25 +9,25 @@ class Client {
     // TODO
     // create a new Ethereum-identity with EthCrypto.createIdentity()
     // - should create a Javascript object with a privateKey, publicKey and address
-    this.wallet = 'EthCrypto identity object';
+    this.wallet = EthCrypto.createIdentity();
   }
 
   // Creates a keccak256/SHA3 hash of some data
   hash(data) {
     // TODO
-    return 'hash of data';
+    return EthCrypto.hash.keccak256(data);
   }
 
   // Signs a hash of data with the client's private key
   sign(data) {
     // TODO
-    return 'signed hash';
+    var dataHash = EthCrypto.hash.keccak256(data);
+    return EthCrypto.sign(this.wallet.privateKey, dataHash);
   }
 
   // Verifies that a messageHash is signed by a certain address
   verify(signature, messageHash, address) {
-    // TODO
-    return 'boolean';
+    return address == EthCrypto.recover(signature, messageHash);
   }
 }
 
